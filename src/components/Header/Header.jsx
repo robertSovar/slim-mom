23bc7c3a9b9923e297f9f0a22cfa188c621c2394
robertSovar <133514926+robertSovar@users.noreply.github.com>
2024-06-27 17:58:25 +0300

    Update logo and mobile version

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,9 @@
+import logoM1 from "./../../assets/images/header-photos/slim-mom-logo-m-1x.webp";
+import logoM2 from "./../../assets/images/header-photos/slim-mom-logo-m-2x.webp";
+import logoT1 from "./../../assets/images/header-photos/slim-mom-logo-t-1x.webp";
+import logoT2 from "./../../assets/images/header-photos/slim-mom-logo-t-2x.webp";
+import logoD1 from "./../../assets/images/header-photos/slim-mom-logo-d-1x.webp";
+import logoD2 from "./../../assets/images/header-photos/slim-mom-logo-d-2x.webp";
 import styles from "./Header.module.css";
 
 function Header() {
@@ -8,21 +14,18 @@ function Header() {
           <picture className={styles.logoSection}>
             <source
               className={styles.logoPictures}
-              srcSet="./assets/images/header-photos/slim-mom-logo-d-1x.webp, ./assets/images/header-photos/slim-mom-logo-d-2x.webp"
+              srcSet={`${logoD1} 1x,${logoD2} 2x`}
               media="(min-width: 1280px)"
             />
             <source
-              srcSet="./assets/images/header-photos/slim-mom-logo-t-1x.webp, ./assets/images/header-photos/slim-mom-logo-t-2x.webp"
+              srcSet={`${logoT1} 1x,${logoT2} 2x`}
               media="(min-width: 321px) and (max-width: 768px)"
             />
             <source
-              srcSet="./assets/images/header-photos/slim-mom-logo-m-1x.webp, ./assets/images/header-photos/slim-mom-logo-m-2x.webp"
+              srcSet={`${logoM1} 1x ,${logoM2} 2x`}
               media="(max-width: 320px)"
             />
-            <img
-              src="./assets/images/header-photos/slim-mom-logo-d-1x.webp"
-              alt="logo"
-            />
+            <img src={logoD1} alt="logo" />
           </picture>
           <span className={styles.slimSpan}>
             Slim{" "}
