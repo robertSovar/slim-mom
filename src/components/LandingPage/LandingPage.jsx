e468baa5930a3c9cb95750a8b7a14b62d5dad516
robertSovar <133514926+robertSovar@users.noreply.github.com>
2024-06-28 19:05:43 +0300

    update landing page for mobile

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,8 +1,77 @@
-
-
+import styles from "./LandingPage.module.css";
 
 function LandingPage() {
-  return <div>This is the landing page</div>;
+  return (
+    <section className={styles.landingPageSection}>
+      <h1 className={styles.landingPageTitle}>
+        Calculate your daily calorie intake right now{" "}
+      </h1>
+      <form action="quiz" className={styles.quizFormContainer}>
+        <label htmlFor="quiz">
+          <input
+            type="number"
+            name="height"
+            id="height"
+            placeholder="Height *"
+            required
+          />
+        </label>
+        <label htmlFor="quiz">
+          <input
+            type="number"
+            name="age"
+            id="age"
+            placeholder="Age *"
+            required
+          />
+        </label>
+        <label htmlFor="quiz">
+          <input
+            type="number"
+            name="current"
+            id="current"
+            placeholder="Current weight *"
+            required
+          />
+        </label>
+        <label htmlFor="quiz">
+          <input
+            type="number"
+            name="desired"
+            id="desired"
+            placeholder="Desired weight *"
+            required
+          />
+        </label>
+        <div className={styles.bloodTypeFormContainer}>
+          <p className={styles.bloodTypeParagraph}>Blood type *</p>
+          <div className={styles.bloodTypeForm}>
+            <label>
+              <input type="radio" name="blood_type" value="1" required />1
+            </label>
+            <br />
+            <label>
+              <input type="radio" name="blood_type" value="2" />2
+            </label>
+            <br />
+            <label>
+              <input type="radio" name="blood_type" value="3" />3
+            </label>
+            <br />
+            <label>
+              <input type="radio" name="blood_type" value="4" />4
+            </label>
+            <br />
+          </div>
+        </div>
+        <div className={styles.bloodTypeBtnContainer}>
+          <button type="submit" className={styles.submitFormBtn}>
+            Start losing weight
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 }
 
-export default LandingPage; 
+export default LandingPage;
