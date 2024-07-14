7468c1a746421e7b3c96538b25de46698dc1ed86
robertSovar <133514926+robertSovar@users.noreply.github.com>
2024-07-14 16:42:30 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "404.html",
-          dest: "",
-        },
-      ],
-    }),
-  ],
-  base: "/slim-mom/", // adaugă baseurl aici
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-      },
-    },
-  },
+  plugins: [react()],
+  base: "/slim-mom/",
 });
