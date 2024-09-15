3933bf6365dccb890d17d70502c5c9d8c8a62a98
robertSovar <133514926+robertSovar@users.noreply.github.com>
2024-09-15 12:27:27 +0300

    Update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import MainPage from "./pages/MainPage/MainPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
@@ -6,8 +7,16 @@ import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 import Layout from "./assets/utils/Layout/Layout";
 import { Provider } from "react-redux";
 import store from "./Store";
+import { useEffect } from "react";
 
 function App() {
+  const history = useHistory();
+  useEffect(() => {
+    const pathname = window.location.search.slice(1);
+    if (pathname) {
+      history.replace(pathname);
+    }
+  }, [history]);
   return (
     <Provider store={store}>
       <Routes>
