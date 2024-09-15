19e00bca336881b21682975f50f3de38d8a61fff
robertSovar <133514926+robertSovar@users.noreply.github.com>
2024-09-15 12:29:56 +0300

    Rollback to prev version

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import { useHistory } from "react-router-dom";
 import MainPage from "./pages/MainPage/MainPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
@@ -7,16 +6,8 @@ import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 import Layout from "./assets/utils/Layout/Layout";
 import { Provider } from "react-redux";
 import store from "./Store";
-import { useEffect } from "react";
 
 function App() {
-  const history = useHistory();
-  useEffect(() => {
-    const pathname = window.location.search.slice(1);
-    if (pathname) {
-      history.replace(pathname);
-    }
-  }, [history]);
   return (
     <Provider store={store}>
       <Routes>
